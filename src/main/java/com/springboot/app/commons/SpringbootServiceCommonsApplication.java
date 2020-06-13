682cc335e979540544922d3b160767dc8fe996de
fraaan <castillofranciscodaniel@gmail.com>
2020-06-13 20:18:47 -0300

    exclucion de la conifg de mongo en la clase principal y agrego un nivel de auditoria mas chico que solo registra el usuario

--- src/main/java/com/springboot/app/commons/SpringbootServiceCommonsApplication.java
+++ src/main/java/com/springboot/app/commons/SpringbootServiceCommonsApplication.java
@@ -2,10 +2,13 @@ package com.springboot.app.commons;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 // para que la dependencia de spring-boot-jpa no nos pida configurar una BD
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, 
+	    MongoDataAutoConfiguration.class })
 public class SpringbootServiceCommonsApplication {
 }
