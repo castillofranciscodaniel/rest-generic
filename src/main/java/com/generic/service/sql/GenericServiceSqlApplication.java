8b3324dc49698eb294ae1d8f7aba85aa9e141718
fraaan <castillofranciscodaniel@gmail.com>
2020-07-25 18:56:57 -0300

    cosas de mongo que quedaban para borrar

--- src/main/java/com/generic/service/sql/GenericServiceSqlApplication.java
+++ src/main/java/com/generic/service/sql/GenericServiceSqlApplication.java
@@ -2,13 +2,8 @@ package com.generic.service.sql;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-// para que la dependencia de spring-boot-jpa no nos pida configurar una BD
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, MongoAutoConfiguration.class, 
-	    MongoDataAutoConfiguration.class })
+@EnableAutoConfiguration
 public class GenericServiceSqlApplication {
 }
